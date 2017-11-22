4659f087140356a6e4163132883274d7fd47ed03
Matthew Welch <dmw9400@gmail.com>
2017-11-22 17:44:54 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
     holidays.each do |holiday, items|
 
         puts "#{holiday.to_s.split each do |holiday|
-                  holiday.capitalize}: #{items}"
+                  holiday.capitalize.join}: #{items}"
 
     end
   end
